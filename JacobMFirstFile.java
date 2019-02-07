257ac870606c434b42b3c6da7aeb8f68978c9e75
zgrkbr <ozgurkibar78@gmail.com>
2019-02-07 11:06:43 +0100

    made changes to Jacobs file

--- JacobMFirstFile.java
+++ JacobMFirstFile.java
@@ -1,6 +1,6 @@
 class JacobMFirstFile{
 	public void printMessage(){
-		System.out.println("Hello, this is a message!");
+		System.out.println("Hello, this is a message from outer space!");
 	}
 }
 
