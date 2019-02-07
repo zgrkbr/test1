53c5693cab48f53aea4ef6f6d8639742a8b1a9e8
Oliverdfs <oliver.hagstrom@gmail.com>
2019-02-07 11:17:25 +0100

    lade till lite onödigt

--- JacobMFirstFile.java
+++ JacobMFirstFile.java
@@ -1,6 +1,11 @@
+
+
 class JacobMFirstFile{
 	public void printMessage(){
 		System.out.println("Hello, this is a message from outer space!");
+
 	}
+	public void printMessage(){
+		System.out.println("..and this is a message from inner cyberspac..and this is a message from inner cyberspacee")
 }
 
